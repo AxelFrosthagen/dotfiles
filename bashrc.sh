6e1b67fb562ec99618c60a55baf13ac7c0c47969
Axel Frosthagen <axel.frosthagen@gmail.com>
2024-10-27 16:34:46 +0100

    Init new dotfiles repo using Babel for installations

---
+++ bashrc.sh
@@ -0,0 +1,16 @@
+#
+# ~/.bashrc
+#
+
+eval "$(starship init bash)"
+export STARSHIP_CONFIG=~/dotfiles/starship.toml
+# PS1='[\u@\h \W]\$ '
+
+
+alias ls='ls --color=auto'
+alias la='ls -a'
+alias ll='ls -l'
+alias la='ls -la'
+alias grep='grep --color=auto'
+
+
